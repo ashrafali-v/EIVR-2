68b8868374e1a85b8a1d212e919bda4fff76e1f6
ashrafali-v <ashruashrafali@gmail.com>
2020-07-14 22:21:19 +0530

    Toggles page update

--- src/app/toggle/toggle.component.ts
+++ src/app/toggle/toggle.component.ts
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ToggleComponent implements OnInit {
 
   constructor() { }
-
+  togglesArray:any = [];
+  currentPage: any = 1;
+  pageSize: number = 10;
   ngOnInit(): void {
+    this.togglesArray = [{"messageKey":"message key1","messageValue":"message value1","status":true},
+    {"messageKey":"message key2","messageValue":"message value2","status":true},
+    {"messageKey":"message key3","messageValue":"message value3","status":false},
+    {"messageKey":"message key4","messageValue":"message value4","status":true},
+    {"messageKey":"message key5","messageValue":"message value5","status":false},
+    {"messageKey":"message key6","messageValue":"message value6","status":false}];
   }
 
 }
